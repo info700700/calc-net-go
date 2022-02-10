70f87cc0fbb53ce0bfe4bfa687cbcc0985cd751f
info700700 <info700700@yandex.ru>
2022-02-10 19:56:28 +0300

    add api handler

--- calc.go
+++ calc.go
@@ -2,8 +2,17 @@ package main
 
 import (
 	"fmt"
+	"html"
+	"log"
+	"net/http"
 )
 
 func main() {
-	fmt.Println("calc")
+	http.HandleFunc("/api/calc", handleCalc)
+
+	log.Fatal(http.ListenAndServe(":80", nil))
+}
+
+func handleCalc(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
 }
