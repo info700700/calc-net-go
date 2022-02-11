ca73a0c8c5e381c0981dad8f077f55c3e1e8df57
info700700 <info700700@yandex.ru>
2022-02-11 15:48:05 +0300

    Add main page

--- calc.go
+++ calc.go
@@ -8,9 +8,30 @@ import (
 )
 
 func main() {
+	http.HandleFunc("/", handleMain)
 	http.HandleFunc("/api/calc", handleCalc)
 
 	log.Fatal(http.ListenAndServe(":80", nil))
+}
+
+const mainPage = `
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Калькулятор</title>
+</head>
+<body>
+  <p>
+    Введите выражение
+  </p>
+  <input>
+</body>
+</html>
+`
+
+func handleMain(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, mainPage)
 }
 
 func handleCalc(w http.ResponseWriter, r *http.Request) {
